14b1a5fe830067a7e49e5271d4a570025ad26da8
Yokiiqrr <1102833828@qq.com>
2023-04-01 21:03:22 -0700

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+// import { MoralisProvider } from "react-moralis";
+// import { NotificationProvider } from "web3uikit";
+// import "../styles/globals.css";
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <MoralisProvider initializeOnMount={false}>
+//       <NotificationProvider>
+//         <Component {...pageProps} />
+//       </NotificationProvider>
+//     </MoralisProvider>
+//   );
+// }
+
+// export default MyApp;
+
+// import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import "../styles/globals.css"
+import { NotificationProvider } from "web3uikit"
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
+}
+
+export default MyApp
